5c9ab35d40f6646a96ab867e713765d4c3b0eed7
Carlos Escobar <escobarcarlos@gmail.com>
2022-04-25 13:40:20 -0700

    Post-Page

--- dex/src/Components/PostArt/PostPage.tsx
+++ dex/src/Components/PostArt/PostPage.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import "./PostPage.scss";
 import UploadSection from "./UploadSection";
+import { Icon } from "@iconify/react";
 
 interface IUser {
   title?: string;
@@ -23,17 +24,15 @@ const PostPage = () => {
     });
   };
 
-  // const artList = (e:any) => { 
-  //   e.preventDefault(); 
+  // const artList = (e:any) => {
+  //   e.preventDefault();
   //   axios
-  //   .post("http://localhost:4000/", { 
+  //   .post("http://localhost:4000/", {
   //     createArt:createArt
   //   })
   //   .catch((err) => console.log(err));
 
   // }
-
-  
 
   return (
     <div className="PostPage-window d-flex">
@@ -57,42 +56,21 @@ const PostPage = () => {
               onChange={(e) => artUpdate({ desc: e.target.value })}
             ></textarea>
           </div>
-          <div className= "upload-section">
-            {/* <UploadSection />  */}
+
+          <div className="choosefile-input">
+            <input type="file" />
           </div>
-         
-          {/* <div className="submit-button">
-            <button type="submit">Submit</button>
-          </div> */}
+
+          <div className="submit-button">
+            <button type="submit">Upload Image</button>
+          </div>
         </form>
+        <div className="blue-box">
+          <Icon className="back-Icon" icon="typcn:arrow-back-outline" />
+          <h1>Back Home</h1>
+        </div>
       </div>
     </div>
-
-    // <div className="PostPage-window d-flex">
-    //   <div className="userPost-text d-flex">
-    //     <h1>Hello PrivateDrippy, upload art for others to see</h1>
-    //   </div>
-    //   <div className="post-forms">
-    //     <form action="">
-    //       <div className="imageTitle-sec">
-    //         <label>Image Title</label>
-    //         <input type="text" placeholder="Image Name" />
-    //       </div>
-    //       <div className="imageDesc-sec">
-    //         <label>Image Description</label>
-    //         <textarea placeholder="Description"></textarea>
-    //       </div>
-    //       <div className="uploadImage-sec">
-    //         <label>Upload Image dsa dsad sad sad sads a</label>
-    //         <input type="file" id="image" name="image" value="" required />
-    //       </div>
-    //       <div>
-    //         <button type="submit">Submit</button>
-    //       </div>
-    //     </form>
-    //   </div>
-    //   <div className="photo-insert"></div>
-    // </div>
   );
 };
 
